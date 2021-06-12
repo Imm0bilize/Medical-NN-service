5239a365e715f7b2504dbbb7788b82db17d5bbe2
Imm0bilize <vst515@yandex.ru>
2021-06-12 18:05:44 +0300

    add post handler

--- src/service/app.py
+++ src/service/app.py
@@ -1,5 +1,7 @@
+import atexit
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 from loguru import logger
 
 
@@ -8,9 +10,34 @@ logger.add('log/debug.log', format='{time} {level} {message}',
            level='DEBUG', rotation='00:00', compression='zip')
 
 
+@app.post('/upload/ct/seg/lung_and_dmg')
+def create_lung_and_damage_seg(file: UploadFile = File(...)):
+    data = file.read()
+
+
+@app.post('/upload/ct/seg/lung')
+def create_lung_seg(file: UploadFile = File(...)):
+    data = file.read()
+
+
+@app.post('/upload/ct/seg/dmg')
+def create_lung_seg(file: UploadFile = File(...)):
+    data = file.read()
+
+
+@app.post('/upload/ct/det/dmg')
+def create_ct_damage_detection(file: UploadFile = File(...)):
+    data = file.read()
+
+
 @app.get('/')
 def start_page():
     return {'info': 'Service working'}
+
+
+@atexit.register
+def stopping_service():
+    logger.debug('Stopping service')
 
 
 @logger.catch
