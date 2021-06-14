eba749151f150ba921958d3275913b2748e8a600
init commit <vst515@yandex.ru>
2021-06-14 10:33:57 +0300

    add config file for NN

---
+++ src/neural_networks/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+IMG_SIZE = 512
+N_CLASSES = 1
+MIN_BOUND = -1000.0
+MAX_BOUND = 500.0
+VALUE_FOR_SEGMENTED_DMG = 1000
+DICOM_BACKGROUND = -2048
+MASK_MERGE_THRESHOLD = 0.1
+
+PATH_TO_LUNG_MODEL_WEIGHTS = os.path.join(os.path.dirname('config.py'), 'weights', 'lung_segmentation_weights.h5')
+PATH_TO_DAMAGE_MODEL_WEIGHTS = os.path.join(os.path.dirname('config.py'), 'weights', 'damage_segment_weights.h5')
