6fc6a956b41e07e764337cafe2188709dc279f73
init commit <vst515@yandex.ru>
2021-06-14 10:35:03 +0300

    Change app logic, adding predict session

--- src/service/app.py
+++ src/service/app.py
@@ -3,41 +3,76 @@ import atexit
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from loguru import logger
+from threading import Timer
+
+from src.neural_networks.prediction import Prediction
 
 
+TIME_TO_SHUTDOWN_SESSION: float = 300.0
+
+timer = None
+pred = None
 app = FastAPI()
 logger.add('log/debug.log', format='{time} {level} {message}',
            level='DEBUG', rotation='00:00', compression='zip')
 
-
-@app.post('/upload/ct/seg/lung_and_dmg')
-def create_lung_and_damage_seg(file: UploadFile = File(...)):
-    data = file.read()
-
-
-@app.post('/upload/ct/seg/lung')
-def create_lung_seg(file: UploadFile = File(...)):
-    data = file.read()
+def set_timer():
+    global timer
+    if timer is not None:
+        timer.cancel()
+    timer = Timer(TIME_TO_SHUTDOWN_SESSION, close_session)
+    timer.start()
 
 
-@app.post('/upload/ct/seg/dmg')
-def create_lung_seg(file: UploadFile = File(...)):
-    data = file.read()
+@app.post('/upload')
+async def create_prediction(file: UploadFile = File(...)):
+    data = await file.read()
+    # prediction = await model.predict(data)
+    set_timer()
 
 
-@app.post('/upload/ct/det/dmg')
-def create_ct_damage_detection(file: UploadFile = File(...)):
-    data = file.read()
+@app.get('/start/{params}')
+def start_session(params: str):
+    """
+    Starting session and timer for auto close session, loading models
+
+    :param params:
+     ct-lung-seg - Segmentation lung on CT,
+     ct-dmg-seg - Segmentation damage on CT,
+     ct-lung-dmg-seg - Segmentation lung and damage on CT and calculate damage percent
+     ct-dmg-det - Detection damage on CT
+    """
+
+    global pred
+
+    try:
+        pred = Prediction(params, logger)
+    except KeyError:
+        logger.error(f'Can`t start session with this params -- {params}')
+        pred = None
+        return {'error': 'Starting session interrupted'}
+
+    set_timer()
+    logger.debug(f'Starting session: {params}')
+    return {'info': 'Session starting'}
 
 
-@app.get('/')
-def start_page():
-    return {'info': 'Service working'}
+@app.get('/close')
+def close_session():
+    global pred
+    pred = None
+    logger.debug('Session stopping')
+    return {'info': 'Session stopping'}
 
 
 @atexit.register
 def stopping_service():
     logger.debug('Stopping service')
+
+
+@app.get('/')
+def start_page():
+    return {'info': 'Service working'}
 
 
 @logger.catch
