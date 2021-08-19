3994c6f7fb6a0ff448a7701fe895280f65fed703
init commit <vst515@yandex.ru>
2021-08-19 10:44:09 +0300

    Update nn utils

--- src/neural_networks/utils.py
+++ src/neural_networks/utils.py
@@ -1,11 +1,19 @@
 import tensorflow as tf
 
-from .config import GPU_LIST
 
-
-def use_multiple_gpu(func):
-    def wrapper(*args, **kwargs):
-        mirrored_strategy = tf.distribute.MirroredStrategy(devices=GPU_LIST)
-        with mirrored_strategy.scope():
-            func(*args, **kwargs)
-    return wrapper
+def get_all_sys_info(logger):
+    logger.debug(f'{"#" * 14 } Sys info {"#" * 14}')
+    for k, v in tf.sysconfig.get_build_info().items():
+        message = f"{k}\t->\t{v}"
+        if v:
+            logger.debug(message)
+        else:
+            logger.warning(message)
+    logger.debug(f'{"#" * 10 } Supportable GPU {"#" * 10}')
+    gpus = tf.config.list_physical_devices("GPU")
+    if gpus:
+        for gpu in tf.config.list_physical_devices("GPU"):
+            logger.debug(f"{gpu}")
+    else:
+        logger.warning("GPU not found")
+    logger.debug("#" * 37)
